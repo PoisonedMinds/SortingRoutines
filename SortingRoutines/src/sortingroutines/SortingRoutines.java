18613d91d57970b72d818e62ee1250e00c0b7638
PoisonedMinds <steven_biro@hotmail.com>
2015-04-16 10:58:16 -0400

    fixed bubble sort

--- SortingRoutines/src/sortingroutines/SortingRoutines.java
+++ SortingRoutines/src/sortingroutines/SortingRoutines.java
@@ -48,12 +48,14 @@ public class SortingRoutines {
         }
     }
 //method that switches the place of 2 numbers in an int array
+
     public static void switchPlace(int nums[], int first, int second) {
         int temp = nums[first];
         nums[first] = nums[second];
         nums[second] = temp;
     }
 //method that calls switchPlaces if a smaller number is found before a larger one
+
     private static void selectionSort(int[] nums) {
         for (int i = 0; i < nums.length - 1; i++) {//cycle through all numbers
             for (int j = i + 1; j < nums.length; j++) {//cycle through all numbers to compare to
@@ -64,20 +66,25 @@ public class SortingRoutines {
         }
     }
 //method that call switchPlace if a smaller number is found after a larger one.
+
     private static void bubbleSort(int[] nums) {
-        boolean check;//check to track when the algorithim is done
-        do {
-            check = false;//reset each time
+        boolean check = false;//check to track when the algorithim is done
+        for (int j = 1; j < nums.length; j++) {
+
             for (int i = 0; i < nums.length - 1; i++) {//cycle through all numbers
                 if (nums[i] > nums[i + 1]) {//compare numbers
                     switchPlace(nums, i, i + 1);//call switchPlace
-                               check=true;//trip the boolean check
+                    check = true;//trip the boolean check
                 }
             }
-        } while (check != true);
+            if (check == false) {
+                break;//break if the method is doing nothing
+            }
+        }
 
     }
 //method that iterates through the array, sorting it relative to the srounding number each time until the array is completely sorted
+
     private static void insertionSort(int[] nums) {
         int num, index;
         for (int i = 1; i < nums.length; i++) {//cycle through the second index to the last index
@@ -94,7 +101,7 @@ public class SortingRoutines {
     }
 
     public static void quickSort(int nums[], int start, int end) {
-                if (start < end) {//continue if the section is greater than 1
+        if (start < end) {//continue if the section is greater than 1
             int numb = split(nums, start, end);//find the place of a number
             quickSort(nums, start, numb - 1);//redo this with the number whoes spot was found
             quickSort(nums, numb + 1, end);
